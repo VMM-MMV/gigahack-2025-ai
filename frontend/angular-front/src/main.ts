4188e9007349db9e901737d1ddc3e3c63c72720d
Arina <arinapereteatcu0@gmail.com>
2025-09-14 12:48:47 +0300

    Added frontend for anonimization

---
+++ frontend/angular-front/src/main.ts
@@ -0,0 +1,10 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    // Add other providers as needed
+  ]
+}).catch(err => console.error(err));
